008cda1400e8ca7d9870add5859da487f1a35e49
Fabricio <fabriciorateni@gmail.com>
2022-01-02 22:39:31 -0300

    css ++

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,28 @@
-import { LiveReload, Outlet, Links } from "remix";
+import type { LinksFunction } from "remix";
+import { Links, LiveReload, Outlet } from "remix";
+
+import globalStylesUrl from "./styles/global.css";
+import globalMediumStylesUrl from "./styles/global-medium.css";
+import globalLargeStylesUrl from "./styles/global-large.css";
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: globalStylesUrl
+    },
+    {
+      rel: "stylesheet",
+      href: globalMediumStylesUrl,
+      media: "print, (min-width: 640px)"
+    },
+    {
+      rel: "stylesheet",
+      href: globalLargeStylesUrl,
+      media: "screen and (min-width: 1024px)"
+    }
+  ];
+};
 
 export default function App() {
   return (
