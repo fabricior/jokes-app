5a32224e510a8dc3b6d89e73474248437c9d0572
Fabricio <fabriciorateni@gmail.com>
2022-01-02 23:34:43 -0300

    read jokes from DB

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,5 +1,7 @@
-import type { LinksFunction } from "remix";
+import { Joke } from "@prisma/client";
+import { LinksFunction, LoaderFunction, useLoaderData } from "remix";
 import { Outlet, Link } from "remix";
+import { db } from "~/utils/db.server";
 import stylesUrl from "../styles/jokes.css";
 
 export const links: LinksFunction = () => {
@@ -11,7 +13,19 @@ export const links: LinksFunction = () => {
   ];
 };
 
+type LoaderData = {
+  jokeListItems: Array<{ id: string; name: string }>;
+};
+
+export const loader: LoaderFunction = async () => {
+  const data: LoaderData = {
+    jokeListItems: await db.joke.findMany()
+  };
+  return data;
+};
+
 export default function JokesRoute() {
+  let data = useLoaderData<LoaderData>();
   return (
     <div className="jokes-layout">
       <header className="jokes-header">
@@ -34,9 +48,12 @@ export default function JokesRoute() {
             <Link to=".">Get a random joke</Link>
             <p>Here are a few more jokes to check out:</p>
             <ul>
-              <li>
-                <Link to="some-joke-id">Hippo</Link>
-              </li>
+            {data.jokeListItems.map(joke => (
+                <li key={joke.id}>
+                  <Link to={joke.id}>{joke.name}</Link>
+                </li>
+              ))}
+            
             </ul>
             <Link to="new" className="button">
               Add your own
