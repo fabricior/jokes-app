fc1dd1d6fab68f7646a1b38bf6b1ce27598dd094
Fabricio <fabriciorateni@gmail.com>
2022-01-07 23:06:21 -0300

    auth - register

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -6,7 +6,7 @@ import {
   useSearchParams
 } from "remix";
 import { db } from "~/utils/db.server";
-import { createUserSession, login } from "~/utils/session.server";
+import { createUserSession, login, register } from "~/utils/session.server";
 import stylesUrl from "../styles/login.css";
 
 export const links: LinksFunction = () => {
@@ -90,12 +90,14 @@ export const action: ActionFunction = async ({
           formError: `User with username ${username} already exists`
         });
       }
-      // create the user
-      // create their session and redirect to /jokes
-      return badRequest({
-        fields,
-        formError: "Not implemented"
-      });
+      const user = await register({ username, password });
+      if (!user) {
+        return badRequest({
+          fields,
+          formError: `Something went wrong trying to create a new user.`
+        });
+      }
+      return createUserSession(user.id, redirectTo);
     }
     default: {
       return badRequest({
