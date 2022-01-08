45f2acffb41effe299f19ed7c2a929720237143e
Fabricio <fabriciorateni@gmail.com>
2022-01-08 13:35:53 -0300

    delete feature

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,11 +1,13 @@
-import type { LoaderFunction } from "remix";
+import { ActionFunction, LoaderFunction, redirect } from "remix";
 import { Link, useLoaderData, useCatch, useParams } from "remix";
 import type { Joke } from "@prisma/client";
 import { db } from "~/utils/db.server";
+import { getUserId, requireUserId } from "~/utils/session.server";
 
-type LoaderData = { joke: Joke };
+type LoaderData = { joke: Joke; isOwner: boolean };
 
-export const loader: LoaderFunction = async ({ params }) => {
+export const loader: LoaderFunction = async ({ request, params }) => {
+  const userId = await getUserId(request);
   const joke = await db.joke.findUnique({
     where: { id: params.jokeId },
   });
@@ -14,8 +16,31 @@ export const loader: LoaderFunction = async ({ params }) => {
       status: 404,
     });
   }
-  const data: LoaderData = { joke };
+  const data: LoaderData = {
+    joke,
+    isOwner: userId === joke.jokesterId,
+  };
   return data;
+};
+
+export const action: ActionFunction = async ({ request, params }) => {
+  const form = await request.formData();
+  if (form.get("_method") === "delete") {
+    const userId = await requireUserId(request);
+    const joke = await db.joke.findUnique({
+      where: { id: params.jokeId },
+    });
+    if (!joke) {
+      throw new Response("Can't delete what does not exist", { status: 404 });
+    }
+    if (joke.jokesterId !== userId) {
+      throw new Response("Pssh, nice try. That's not your joke", {
+        status: 401,
+      });
+    }
+    await db.joke.delete({ where: { id: params.jokeId } });
+    return redirect("/jokes");
+  }
 };
 
 export default function JokeRoute() {
@@ -26,6 +51,18 @@ export default function JokeRoute() {
       <p>Here's your hilarious joke:</p>
       <p>{data.joke.content}</p>
       <Link to=".">{data.joke.name} Permalink</Link>
+      {data.isOwner ? (
+        <form method="post">
+          <input
+            type="hidden"
+            name="_method"
+            value="delete"
+          />
+          <button type="submit" className="button">
+            Delete
+          </button>
+        </form>
+      ) : null}
     </div>
   );
 }
